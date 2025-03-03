0edd0c9aedb1603ee86bc9f1185d2ca6b04902bd
claudiopb <hai_madre@hotmail.com>
2025-03-03 11:16:22 -0600

    su

---
+++ JSON_PY/OBaJSON.py
@@ -0,0 +1,31 @@
+import json
+
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
+
+'''
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x))
+'''
